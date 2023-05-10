20c5c72f73bc1fd6077eb4d8373fd71ccb7332de
Dog2077 <1002302241@qq.com>
2023-05-10 22:47:42 +0800

    Learn javaweb

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,14 +1,30 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class MyClient {
     public static void main(String[] args) {
         try (Socket myClient = new Socket("localhost", 8080)) {
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Successfully connect to MyServer!");
+            while (true) {
+                OutputStream outputStream = myClient.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+                System.out.println("Please input a number:");
+                String information = scanner.nextLine();
+                outputStreamWriter.write(information + '\n');
+                outputStreamWriter.flush();
+                System.out.println("The information has sent to MyServer: " + information);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(myClient.getInputStream()));
+                String reinformation = bufferedReader.readLine();
+                if(reinformation.equals("False")) break;
+            }
         } catch (IOException e) {
             System.out.println("Failed connect to MyServer.");
+        } finally {
+            System.out.println("Tht connection has interpreted.");
         }
     }
 }
--- src/MyServer.java
+++ src/MyServer.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +11,30 @@ public class MyServer {
             System.out.println("Waiting for Client to connect...");
             Socket connectStatus = myServer.accept();
             System.out.println("Connection accessible, the Client ip :" + connectStatus.getInetAddress());
+            while (true) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connectStatus.getInputStream()));
+                String information = bufferedReader.readLine();
+                int num;
+                try {
+                    num = Integer.parseInt(information);
+                } catch (NumberFormatException e){
+                    System.out.println(information + "is not a number.");
+                    break;
+                }
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connectStatus.getOutputStream());
+                if (num > 0){
+                    System.out.println(num + " is an active number.");
+                    outputStreamWriter.write("True\n");
+                }
+                else {
+                    System.out.println(num + " is a negative number.");
+                    outputStreamWriter.write("False\n");
+                    break;
+                }
+                outputStreamWriter.flush(); // 刷新输出流
+            }
+            myServer.close();
+            System.out.println("The connection has interpreted.");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
