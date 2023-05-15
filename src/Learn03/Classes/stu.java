d6cab506915aba610585061c54ce1cd3386306d8
Dog2077 <1002302241@qq.com>
2023-05-15 16:53:44 +0800

    Add mappers

---
+++ src/Learn03/Classes/stu.java
@@ -0,0 +1,14 @@
+package Learn03.Classes;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class stu {
+    Integer Sid;
+    String Sname;
+    String Ssex;
+}
--- src/Learn03/Main.java
+++ src/Learn03/Main.java
@@ -1,9 +1,23 @@
 package Learn03;
 import Learn03.Classes.Vtuber;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 //        Vtuber vtuber = new Vtuber("Hiiro", "cat", 114);
 //        System.out.println(vtuber);
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(new FileInputStream("config/mybatis-config.xml"));
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)){
+            List<Object> selectStu = sqlSession.selectList("selectStu");
+            selectStu.forEach(System.out::println);
+        }
+
     }
 }
