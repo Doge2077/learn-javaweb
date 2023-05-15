56b1503a36c56887020767e23869d891930780cc
Dog2077 <1002302241@qq.com>
2023-05-15 16:11:53 +0800

    Add mybatis-config.xml

--- src/Learn03/Classes/Vtuber.java
+++ src/Learn03/Classes/Vtuber.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+
+// lombook 注解在编译期修改语法树生成对应的构造函数和方法
 @Setter
 @Getter
 @ToString
--- src/Learn03/Main.java
+++ src/Learn03/Main.java
@@ -3,7 +3,7 @@ import Learn03.Classes.Vtuber;
 
 public class Main {
     public static void main(String[] args) {
-        Vtuber vtuber = new Vtuber("Hiiro", "cat", 114);
-        System.out.println(vtuber);
+//        Vtuber vtuber = new Vtuber("Hiiro", "cat", 114);
+//        System.out.println(vtuber);
     }
 }
