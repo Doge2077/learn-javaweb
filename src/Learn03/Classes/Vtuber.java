0999503f820f9390a37ed1842536246dbc91732d
Dog2077 <1002302241@qq.com>
2023-05-15 15:52:15 +0800

    Learn lombook and mybatis.

---
+++ src/Learn03/Classes/Vtuber.java
@@ -0,0 +1,16 @@
+package Learn03.Classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class Vtuber {
+    String name;
+    String sex;
+    Integer age;
+}
---
+++ src/Learn03/Main.java
@@ -0,0 +1,9 @@
+package Learn03;
+import Learn03.Classes.Vtuber;
+
+public class Main {
+    public static void main(String[] args) {
+        Vtuber vtuber = new Vtuber("Hiiro", "cat", 114);
+        System.out.println(vtuber);
+    }
+}
