6ccbf4e76b8f9c8203adf29ab297639452f7902c
Dog2077 <1002302241@qq.com>
2023-05-19 00:50:58 +0800

    Add Maven base

---
+++ Learn_JavaWeb02/src/main/java/Learn01/Classes/Vtuber.java
@@ -0,0 +1,13 @@
+package Learn01.Classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Vtuber {
+    String name;
+    String sex;
+    Integer age;
+    Boolean reborn;
+}
---
+++ Learn_JavaWeb02/src/main/java/Learn01/Main.java
@@ -0,0 +1,11 @@
+package Learn01;
+
+import Learn01.Classes.Vtuber;
+import lombok.val;
+
+public class Main {
+    public static void main(String[] args) {
+        Vtuber vtuber = new Vtuber("Hiiro", "Cat", 114514, false);
+        System.out.println(vtuber);
+    }
+}
