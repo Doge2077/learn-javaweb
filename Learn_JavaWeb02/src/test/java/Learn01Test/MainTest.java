11a5565dcb06ffb117e4bc76a8d9c89106fc4a50
Dog2077 <1002302241@qq.com>
2023-05-19 10:37:06 +0800

    Add ChileProject01

--- Learn_JavaWeb02/src/test/java/Learn01Test/MainTest.java
+++ Learn_JavaWeb02/src/test/java/Learn01Test/MainTest.java
@@ -1,16 +1,36 @@
 package Learn01Test;
 
+import Learn01.Mappers.Select01;
+import lombok.SneakyThrows;
 import lombok.extern.java.Log;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.*;
 
 @Log
 public class MainTest {
 
-    @Test
+    //因为配置文件位于内部，我们需要使用Resources类的getResourceAsStream来获取内部的资源文件
+    private static SqlSessionFactory factory;
+
+    //在JUnit5中@Before被废弃，它被细分了：
+    @BeforeAll // 一次性开启所有测试案例只会执行一次 (方法必须是static)
+    // @BeforeEach 一次性开启所有测试案例每个案例开始之前都会执行一次
+    @SneakyThrows
+    public static void before(){
+        factory = new SqlSessionFactoryBuilder()
+                .build(Resources.getResourceAsStream("mybatis-config.xml"));
+    }
+
+
+    @DisplayName("Mybatis数据库测试")  //自定义测试名称
+    @RepeatedTest(3)  //自动执行多次测试
     public void test(){
-        System.out.println("测试");
-        //Assert在JUnit5时名称发生了变化Assertions
-        Assertions.assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 2});
+        try (SqlSession sqlSession = factory.openSession(true)){
+            Select01 mapper = sqlSession.getMapper(Select01.class);
+            System.out.println(mapper.getAStudentBySid(2));
+        }
     }
 }
