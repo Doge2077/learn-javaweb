30c91b0d139416b5a3e6b7ed58fc75963443ea70
Dog2077 <1002302241@qq.com>
2023-05-24 16:32:35 +0800

    Relearn maven

--- Learn_JavaWeb02/src/main/java/Learn01/Mappers/Select01.java
+++
@@ -1,9 +0,0 @@
-package Learn01.Mappers;
-
-import Learn01.Classes.Student;
-import org.apache.ibatis.annotations.Select;
-
-public interface Select01 {
-    @Select("SELECT * FROM stu WHERE Sid = #{Sid}")
-    Student getAStudentBySid(Integer Sid);
-}
