1aa7011fd76b39b364b3f32d1dad2f4bc64ae404
Dog2077 <1002302241@qq.com>
2023-05-25 17:28:05 +0800

    Add child02 in child01.xml

---
+++ Learn_Javaweb02/Child01/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import classes.Student;
+
+import java.lang.System;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student(1, "Hiiro", "cat");
+        System.out.println(student);
+    }
+
+}
---
+++ Learn_Javaweb02/Child02/src/main/java/classes/Student.java
@@ -0,0 +1,12 @@
+package classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    Integer Sid;
+    String Sname;
+    String Ssex;
+}
