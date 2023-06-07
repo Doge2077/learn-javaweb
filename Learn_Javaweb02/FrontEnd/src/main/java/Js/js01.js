883e3006dbfd3d067f6dc4c1415914771b716576
Dog2077 <1002302241@qq.com>
2023-06-07 15:58:26 +0800

    Add login html css

---
+++ Learn_Javaweb02/FrontEnd/src/main/java/Js/js01.js
@@ -0,0 +1,5 @@
+const arr = [0, 5, 3, 4, 1, 2]
+
+for (let i = 0; i < arr.length; i ++) {
+    console.log(arr[i])
+}
---
+++ Learn_Javaweb02/FrontEnd/src/main/java/Js/page01.js
@@ -0,0 +1,5 @@
+let info = "Fxxk Js shit ";
+
+for (let i = 1; ; i ++) {
+    alert(info + i + " times!")
+}
